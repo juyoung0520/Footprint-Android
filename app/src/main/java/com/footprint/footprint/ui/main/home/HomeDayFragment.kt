6834c3b26267b23c8a33880390edfc1d5c313422
yusiny <yusinkim.or@gmail.com>
2022-01-12 05:03:54 +0900

    Merged upstream/develop & feature/home

--- app/src/main/java/com/footprint/footprint/ui/main/home/HomeDayFragment.kt
+++ app/src/main/java/com/footprint/footprint/ui/main/home/HomeDayFragment.kt
@@ -1,11 +1,18 @@
 package com.footprint.footprint.ui.main.home
 
+import com.dinuscxj.progressbar.CircleProgressBar
 import com.footprint.footprint.databinding.FragmentHomeDayBinding
 import com.footprint.footprint.ui.BaseFragment
 
-class HomeDayFragment:  BaseFragment<FragmentHomeDayBinding>(FragmentHomeDayBinding::inflate)  {
+class HomeDayFragment:  BaseFragment<FragmentHomeDayBinding>(FragmentHomeDayBinding::inflate) , CircleProgressBar.ProgressFormatter {
 
     override fun initAfterBinding() {
+             var circleProgressBar = binding.homeDayPb
+             circleProgressBar.progress = 80
 
+    }
+
+    override fun format(progress: Int, max: Int): CharSequence {
+        return String.format("%d%", (progress.toFloat()/max.toFloat()*100).toInt())
     }
 }
