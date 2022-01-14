300358974b26793a27769c967014e8a09dace1df
EunhaKyeong <kyeongeh424@gmail.com>
2022-01-14 22:37:41 +0900

    Merged! feature/stop-walk-post & upstream/develop

--- app/src/main/java/com/footprint/footprint/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/footprint/footprint/ui/main/home/HomeFragment.kt
@@ -1,23 +1,12 @@
 package com.footprint.footprint.ui.main.home
 
-import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.ForegroundColorSpan
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
-import com.footprint.footprint.R
 import com.footprint.footprint.databinding.FragmentHomeBinding
 import com.footprint.footprint.ui.BaseFragment
-import com.footprint.footprint.ui.main.MainActivity
 
 class HomeFragment(): BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
     override fun initAfterBinding() {
         binding.homeContentTv.setOnClickListener {
             val mainActivity = activity as MainActivity
@@ -25,5 +14,9 @@ class HomeFragment(): BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inf
         }
     }
 
-
+    /*private fun setMyClickListener() {
+        binding.homeContentTv.setOnClickListener {
+            startActivity(Intent(requireContext(), WalkAfterActivity::class.java))
+        }
+    }*/
 }
---
+++ app/src/main/java/com/footprint/footprint/ui/main/home/WalkAfterActivity.kt
@@ -0,0 +1,14 @@
+package com.footprint.footprint.ui.main.home
+
+import com.footprint.footprint.databinding.ActivityWalkAfterBinding
+import com.footprint.footprint.ui.BaseActivity
+
+class WalkAfterActivity : BaseActivity<ActivityWalkAfterBinding>(ActivityWalkAfterBinding::inflate) {
+    override fun initAfterBinding() {
+        setUI()
+    }
+
+    private fun setUI() {
+
+    }
+}
