a5ad68ea3934be9cde80c29a2b61fd1ae06b07d1
yusiny <yusinkim.or@gmail.com>
2022-01-27 16:07:29 +0900

    improve: [수정] RegisterInfo 입력 로직 ver.2

--- app/src/main/java/com/footprint/footprint/ui/register/info/RegisterInfoFragment.kt
+++ app/src/main/java/com/footprint/footprint/ui/register/info/RegisterInfoFragment.kt
@@ -42,8 +42,12 @@ class RegisterInfoFragment() :
     private var isGenderCorrect = false
     private val THIS_YEAR = 2022
     private lateinit var animation: Animation
+    private var date = arrayOf(0, 0, 0)
+    // 0: 입력 안됨, 1: 입력됨 4: 틀림
 
     override fun initAfterBinding() {
+        animation = AnimationUtils.loadAnimation(activity, R.anim.shake)
+
         //Activity에서 newUser 받아오기
         if (arguments != null) {
             tmpUser = arguments?.getSerializable("user") as User
@@ -64,10 +68,10 @@ class RegisterInfoFragment() :
 
         /*버튼 활성화 & 눌렀을 때*/
         binding.registerInfoActionBtn.setOnClickListener {
-            animation = AnimationUtils.loadAnimation(activity, R.anim.shake)
+
 
             //Nickname Validation
-            val validatedNinckname = nicknameValidation()
+            //val validatedNinckname = nicknameValidation()
 
             //Gender Validation
 
@@ -81,7 +85,8 @@ class RegisterInfoFragment() :
                 binding.registerInfoWeightEt.text.toString().toInt()
 
             //ok -> 목표 프래그먼트 데이터 전달
-            if (validatedNinckname && validatedBirth) {
+            if (validatedBirth) {
+                Toast.makeText(activity, "정보 등록됨", Toast.LENGTH_SHORT).show()
                 Log.d("VALIDATION", newUser.toString())
             }
         }
@@ -90,7 +95,7 @@ class RegisterInfoFragment() :
         //keyboardUp(balloon)
     }
 
-    
+
     /*Tool*/
     fun dpToSp(dp: Float, context: Context): Int {
         return (dpToPx(dp, context) / context.resources.displayMetrics.scaledDensity).toInt()
@@ -107,6 +112,10 @@ class RegisterInfoFragment() :
 
     /*Button*/
     private fun checkBtnState() {
+        /*var isBirthCorrect = false
+        //셋 다 잘 입력(1)거나, 셋다 아예 입력x(0) 여야만 true 나머지는 false
+        if((date[0] == 1 && date[1] == 1 && date[2] == 1 ) || (date[0] == 0 && date[1] == 0 && date[2] == 0)) isBirthCorrect = true*/
+
         if (isNicknameCorrect && isGenderCorrect) {
             //버튼 색 바뀌게
             Log.d("TOACTIVITY", tmpUser.toString())
@@ -139,9 +148,11 @@ class RegisterInfoFragment() :
                     ColorStateList.valueOf(resources.getColor(R.color.primary))
                 nicknameTextWatcher()
             } else {
-                if (nicknameEt.text.isEmpty())
+                if (nicknameEt.text.isEmpty()) {
                     nicknameEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.white_underline))
+                    isNicknameCorrect = false
+                }
 
             }
         }
@@ -152,15 +163,27 @@ class RegisterInfoFragment() :
         nicknameEt.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable) {
 
-                //닉네임에 반영
-                newUser.nickname = nicknameEt.text.toString()
-                Log.d("WATCHER/NICKNAME", newUser.toString())
+                if (nicknameEt.text.length > 8) {
+                    //if(nicknameEt.text.length == 9){
+                    //nicknameEt.startAnimation(animation)
+                    //nicknameEt.requestFocus()
+                    nicknameEt.backgroundTintList =
+                        ColorStateList.valueOf(resources.getColor(R.color.secondary))
+                    binding.registerInfoNicknameErrorTv.visibility = View.VISIBLE
+                    isNicknameCorrect = false
 
-                //입력정보 기록 & button State check
-                isNicknameCorrect = nicknameEt.text.isNotEmpty()
-
+                } else {
+                    if (nicknameEt.text.isNotEmpty()) {
+                        //닉네임에 반영
+                        newUser.nickname = nicknameEt.text.toString()
+                        Log.d("WATCHER/NICKNAME", newUser.toString())
+                        nicknameEt.backgroundTintList =
+                            ColorStateList.valueOf(resources.getColor(R.color.primary))
+                        binding.registerInfoNicknameErrorTv.visibility = View.INVISIBLE
+                        isNicknameCorrect = true
+                    }
+                }
                 checkBtnState()
-
             }
 
             override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {}
@@ -188,6 +211,7 @@ class RegisterInfoFragment() :
     private fun birthEt() {
         //year
         val yearEt = binding.registerInfoBirthYearEt
+        //var year: Int? = null
         yearEt.onFocusChangeListener = OnFocusChangeListener { view, hasFocus ->
             if (hasFocus) {
                 // Focus
@@ -196,10 +220,12 @@ class RegisterInfoFragment() :
                 binding.registerInfoBirthYearUnitTv.setTextColor(resources.getColor(R.color.primary))
                 yearTextWatcher()
             } else {
+                //Focus 떼졌을 때
                 if (yearEt.text.isEmpty()) {
                     yearEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.white_underline))
                     binding.registerInfoBirthYearUnitTv.setTextColor(resources.getColor(R.color.primary_light))
+                    //date[0] = 0
                 }
 
             }
@@ -207,6 +233,7 @@ class RegisterInfoFragment() :
 
         //month
         val monthEt = binding.registerInfoBirthMonthEt
+        //var month: Int? = null
         monthEt.onFocusChangeListener = OnFocusChangeListener { view, hasFocus ->
             if (hasFocus) {
                 // Focus
@@ -215,16 +242,19 @@ class RegisterInfoFragment() :
                 binding.registerInfoBirthMonthUnitTv.setTextColor(resources.getColor(R.color.primary))
                 monthTextWatcher()
             } else {
+                // No Focus
                 if (monthEt.text.isEmpty()) {
                     monthEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.white_underline))
                     binding.registerInfoBirthMonthUnitTv.setTextColor(resources.getColor(R.color.primary_light))
+                    //date[1] = 0
                 }
             }
         }
 
         //day
         val dayEt = binding.registerInfoBirthDayEt
+        //var day: Int? = null
         dayEt.onFocusChangeListener = OnFocusChangeListener { view, hasFocus ->
             if (hasFocus) {
                 // Focus
@@ -233,10 +263,12 @@ class RegisterInfoFragment() :
                 binding.registerInfoBirthDayUnitTv.setTextColor(resources.getColor(R.color.primary))
                 dayTextWatcher()
             } else {
+                // Focus X
                 if (dayEt.text.isEmpty()) {
                     dayEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.white_underline))
                     binding.registerInfoBirthDayUnitTv.setTextColor(resources.getColor(R.color.primary_light))
+                    //date[2] = 0
                 }
             }
         }
@@ -249,9 +281,25 @@ class RegisterInfoFragment() :
         yearEt.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable) {
                 if (yearEt.text.isNotEmpty()) {
+                    /* //안 비었는데? -> validation 1900~작년(2021)
+                     val year = parseInt(yearEt.text.toString())
+                     if (year in 1900 until THIS_YEAR) {
+                         //잘 입력됨 -> year ON*/
                     yearEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.primary))
                     binding.registerInfoBirthYearUnitTv.setTextColor(resources.getColor(R.color.primary))
+                    /*     date[0] = 1
+                     } else {
+                         //잘못 입력됨 -> year OFF
+                         date[0] = 4
+                         //떨고 & 색 바뀌고
+                         yearEt.startAnimation(animation)
+                         yearEt.backgroundTintList =
+                             ColorStateList.valueOf(resources.getColor(R.color.secondary))
+                         binding.registerInfoBirthYearUnitTv.setTextColor(resources.getColor(R.color.secondary))
+                     }
+
+                     checkBtnState()*/
                 }
             }
 
@@ -265,9 +313,24 @@ class RegisterInfoFragment() :
         monthEt.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable) {
                 if (monthEt.text.isNotEmpty()) {
+/*
+                    // No Focus & Not Empty -> month Validation: 1~12
+                    val month = parseInt(monthEt.text.toString())
+                    if (month in 1..12) {
+                        // 통과*/
                     monthEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.primary))
                     binding.registerInfoBirthMonthUnitTv.setTextColor(resources.getColor(R.color.primary))
+                    /*      date[1] = 1
+                      } else {
+                          // 실패
+                          date[1] = 4
+                          monthEt.startAnimation(animation)
+                          monthEt.backgroundTintList =
+                              ColorStateList.valueOf(resources.getColor(R.color.secondary))
+                          binding.registerInfoBirthMonthUnitTv.setTextColor(resources.getColor(R.color.secondary))
+                      }
+                      checkBtnState()*/
                 }
             }
 
@@ -281,11 +344,23 @@ class RegisterInfoFragment() :
         dayEt.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable) {
                 if (dayEt.text.isNotEmpty()) {
+/*
+                    val day = parseInt(dayEt.text.toString())
+                    // Day validation: 1~31
+                    if(day in 1 .. 31){*/
                     dayEt.backgroundTintList =
                         ColorStateList.valueOf(resources.getColor(R.color.primary))
                     binding.registerInfoBirthDayUnitTv.setTextColor(resources.getColor(R.color.primary))
+                    /*     date[2] = 1
+                     }else{
+                         date[2] = 4
+                         dayEt.startAnimation(animation)
+                         dayEt.backgroundTintList =
+                             ColorStateList.valueOf(resources.getColor(R.color.secondary))
+                         binding.registerInfoBirthDayUnitTv.setTextColor(resources.getColor(R.color.secondary))
+                     }
+                     checkBtnState()*/
                 }
-
             }
 
             override fun beforeTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {}
@@ -390,13 +465,13 @@ class RegisterInfoFragment() :
         var results = arrayOf(0, 0, 0)
         if (yearEt.text.toString() != "") {
             year = parseInt(yearEt.text.toString())
-            if (year >= THIS_YEAR) {
+            if (year !in 1900 until THIS_YEAR) {
                 yearEt.startAnimation(animation)
                 yearEt.requestFocus()
                 yearEt.backgroundTintList =
                     ColorStateList.valueOf(resources.getColor(R.color.secondary))
                 binding.registerInfoBirthYearUnitTv.setTextColor(resources.getColor(R.color.secondary))
-                results[0] = 0
+                results[0] = 4
             } else results[0] = 1
         }
 
@@ -410,7 +485,7 @@ class RegisterInfoFragment() :
                 monthEt.backgroundTintList =
                     ColorStateList.valueOf(resources.getColor(R.color.secondary))
                 binding.registerInfoBirthMonthUnitTv.setTextColor(resources.getColor(R.color.secondary))
-                results[1] = 0
+                results[1] = 4
             } else results[1] = 1
         }
 
@@ -424,29 +499,48 @@ class RegisterInfoFragment() :
                 dayEt.backgroundTintList =
                     ColorStateList.valueOf(resources.getColor(R.color.secondary))
                 binding.registerInfoBirthDayUnitTv.setTextColor(resources.getColor(R.color.secondary))
-                results[2] = 0
+                results[2] = 4
             } else results[2] = 1
         }
 
-        if (year != null && month != null && day != null) {
-            if (results[0] == 1 && results[1] == 1 && results[2] == 1) {
-                newUser.birthday = String.format("%04d.%02d.%02d", year, month, day)
 
-                yearEt.backgroundTintList =
-                    ColorStateList.valueOf(resources.getColor(R.color.primary))
-                monthEt.backgroundTintList =
-                    ColorStateList.valueOf(resources.getColor(R.color.primary))
-                dayEt.backgroundTintList =
-                    ColorStateList.valueOf(resources.getColor(R.color.primary))
+        if (results[0] == 1 && results[1] == 1 && results[2] == 1) {
+            newUser.birthday = String.format("%04d.%02d.%02d", year, month, day)
 
-                return true
-            }
-        } else if (year == null && month == null && day == null) {
+            yearEt.backgroundTintList =
+                ColorStateList.valueOf(resources.getColor(R.color.primary))
+            monthEt.backgroundTintList =
+                ColorStateList.valueOf(resources.getColor(R.color.primary))
+            dayEt.backgroundTintList =
+                ColorStateList.valueOf(resources.getColor(R.color.primary))
+            return true
+        } else if (results[0] == 0 && results[1] == 0 && results[2] == 0) {
             newUser.birthday = null
             return true
-        } else
-            Log.d("BIRTHDAY/ERROR", "ERROR")
+        }
 
+        //하나라도 입력이 안됨!
+        if (year == null) {
+            yearEt.startAnimation(animation)
+            yearEt.requestFocus()
+            yearEt.backgroundTintList =
+                ColorStateList.valueOf(resources.getColor(R.color.secondary))
+            binding.registerInfoBirthYearUnitTv.setTextColor(resources.getColor(R.color.secondary))
+        }
+        if (month == null) {
+            monthEt.startAnimation(animation)
+            monthEt.requestFocus()
+            monthEt.backgroundTintList =
+                ColorStateList.valueOf(resources.getColor(R.color.secondary))
+            binding.registerInfoBirthMonthUnitTv.setTextColor(resources.getColor(R.color.secondary))
+        }
+        if (day == null) {
+            dayEt.startAnimation(animation)
+            monthEt.requestFocus()
+            dayEt.backgroundTintList =
+                ColorStateList.valueOf(resources.getColor(R.color.secondary))
+            binding.registerInfoBirthDayUnitTv.setTextColor(resources.getColor(R.color.secondary))
+        }
         return false
     }
 
