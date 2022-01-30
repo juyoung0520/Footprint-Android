1dbb9163e04b85af51a8b6666045f7655687c1b0
JJY <juyoung0520@naver.com>
2022-01-31 00:46:14 +0900

    design : [추가] 커스텀 토스트 메시지 추가

--- app/src/main/java/com/footprint/footprint/ui/walk/WalkActivity.kt
+++ app/src/main/java/com/footprint/footprint/ui/walk/WalkActivity.kt
@@ -6,9 +6,12 @@ import com.footprint.footprint.R
 import com.footprint.footprint.databinding.ActivityWalkBinding
 import com.footprint.footprint.ui.BaseActivity
 import com.footprint.footprint.ui.dialog.ActionDialogFragment
+import com.skydoves.balloon.*
 
 class WalkActivity : BaseActivity<ActivityWalkBinding>(ActivityWalkBinding::inflate) {
     override fun initAfterBinding() {
+        setToastMessage()
+
         //취소 텍스트뷰 클릭 리스너 -> 실시간 기록을 중지할까요? 다이얼로그 화면 띄우기
         binding.walkCancelTv.setOnClickListener {
             val bundle: Bundle = Bundle()
@@ -31,6 +34,33 @@ class WalkActivity : BaseActivity<ActivityWalkBinding>(ActivityWalkBinding::infl
                 override fun action2(isAction: Boolean) {
                 }
             })
+        }
+    }
+
+    private fun setToastMessage() {
+        val toastCustom = Balloon.Builder(this)
+            .setText(getString(R.string.msg_question))
+            .setTextColor(getColor(R.color.white))
+            .setWidth(250)
+            .setHeight(55)
+            .setMarginRight(15)
+            .setTextTypeface(R.font.namusquareround)
+            .setIsVisibleArrow(true)
+            .setArrowSize(8)
+            .setArrowOrientation(ArrowOrientation.BOTTOM)
+            .setArrowPosition(0.9f)
+            .setAutoDismissDuration(3000L)
+            .setCornerRadius(40F)
+            .setBackgroundColorResource(R.color.black_80)
+            .setBalloonAnimation(BalloonAnimation.ELASTIC)
+            .build()
+
+        binding.walkHelpIv.setOnClickListener {
+            binding.walkHelpIv.showAlignBottom(toastCustom, 0, 0)
+        }
+
+        toastCustom.setOnBalloonClickListener {
+            toastCustom.dismiss()
         }
     }
 }
