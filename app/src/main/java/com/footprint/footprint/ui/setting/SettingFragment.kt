15f08387ede3e9b48cf066dc61140d8026081167
EunhaKyeong <kyeongeh424@gmail.com>
2022-02-01 01:59:18 +0900

    Rename: 파일 이동 - setting 패키지 추가

--- app/src/main/java/com/footprint/footprint/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/footprint/footprint/ui/setting/SettingFragment.kt
@@ -1,10 +1,6 @@
-package com.footprint.footprint.ui.main.home
+package com.footprint.footprint.ui.setting
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.footprint.footprint.R
 import com.footprint.footprint.databinding.FragmentSettingBinding
