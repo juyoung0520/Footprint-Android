d3fb1d35fc7d6cab0dada75e8ff8bb010a969fd8
EunhaKyeong <kyeongeh424@gmail.com>
2022-02-02 22:19:56 +0900

    Improve: 변경-서버 API 에 따라 목표 데이터 타입 수정으로 인한 코드 수정
    - 목표 요일 데이터: ArrayList<String> -> ArrayList<Int>
    - 목표 산책 시간: 00시간 00분(String) -> 분(Int)
    - 산책 시간대: String -> 인덱스 형태(Int)

--- app/src/main/java/com/footprint/footprint/ui/adapter/DayRVAdapter.kt
+++ app/src/main/java/com/footprint/footprint/ui/adapter/DayRVAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.footprint.footprint.R
 import com.footprint.footprint.databinding.ItemDayBinding
 
 class DayRVAdapter(private val size: Int) : RecyclerView.Adapter<DayRVAdapter.DayViewHolder>() {
@@ -15,18 +14,12 @@ class DayRVAdapter(private val size: Int) : RecyclerView.Adapter<DayRVAdapter.Da
     }
 
     private lateinit var myItemClickListener: MyItemClickListener
-
-    private val day = arrayListOf<Int>(
-        R.string.title_mon_kr,
-        R.string.title_tue_kr,
-        R.string.title_wed_kr,
-        R.string.title_thu_kr,
-        R.string.title_fri_kr,
-        R.string.title_sat_kr,
-        R.string.title_sun_kr
-    )
-
     private lateinit var binding: ItemDayBinding
+
+    private var isEnabled: Boolean = true
+
+    private val day = arrayListOf<String>("월", "화", "수", "목", "금", "토", "일")
+    private val userGoalDay: ArrayList<Int> = arrayListOf()  //사용자 목표 요일(이번달 목표 화면에서 사용)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayRVAdapter.DayViewHolder {
         binding = ItemDayBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -41,16 +34,22 @@ class DayRVAdapter(private val size: Int) : RecyclerView.Adapter<DayRVAdapter.Da
         params.height = size
         holder.day.layoutParams = params
 
-        holder.day.setText(day[position])   //텍스트 바인딩(월화수목금토일)
+        //아이템뷰 활성화/비활성화 설정 -> 이번달 목표 조회일 경우 비활성화
+        holder.day.isEnabled = isEnabled
+
+        holder.day.text = day[position]   //텍스트 바인딩(월화수목금토일)
+
+        //이번달 목표 화면의 경우 사용자가 선택한 목표 요일은 선택된 상태로 보여야 한다.
+        holder.day.isSelected = userGoalDay.isNotEmpty() && userGoalDay.contains(position+1)
 
         //클릭 리스너 -> 클릭됨: 하늘색, 클릭 안됨: 하얀색
         holder.day.setOnClickListener {
             if (it.isSelected) {
                 it.isSelected = false
-                myItemClickListener.removeDay(day[position])
+                myItemClickListener.removeDay(position+1)
             } else {
                 it.isSelected = true
-                myItemClickListener.saveDay(day[position])
+                myItemClickListener.saveDay(position+1)
             }
         }
     }
@@ -59,6 +58,14 @@ class DayRVAdapter(private val size: Int) : RecyclerView.Adapter<DayRVAdapter.Da
 
     fun setMyItemClickListener(myItemClickListener: MyItemClickListener) {
         this.myItemClickListener = myItemClickListener
+    }
+
+    fun setEnabled(isEnabled: Boolean) {
+        this.isEnabled = isEnabled
+    }
+
+    fun setUserGoalDay(userGoalDay: ArrayList<Int>) {
+        this.userGoalDay.addAll(userGoalDay)
     }
 
     inner class DayViewHolder(itemView: ItemDayBinding) : RecyclerView.ViewHolder(itemView.root) {
