8d26e8ec4f921a2a4f724ff45a882c6c38a48613
JJY <juyoung0520@naver.com>
2022-02-05 01:35:30 +0900

    design: [고침] 주 산책 통계 막대 그래프 커스텀
    - 막대 모서리 둥글게 커스텀
    - label 글씨 설정, 격자 설정 등 그래프 설정
    - 데이터에 맞게 막대 데이터 설정

--- app/src/main/java/com/footprint/footprint/ui/main/mypage/MyPageFragment.kt
+++ app/src/main/java/com/footprint/footprint/ui/main/mypage/MyPageFragment.kt
@@ -2,13 +2,13 @@ package com.footprint.footprint.ui.main.mypage
 
 import android.text.Spannable
 import android.text.SpannableString
-import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.footprint.footprint.R
 import com.footprint.footprint.databinding.FragmentMypageBinding
 import com.footprint.footprint.ui.BaseFragment
+import com.footprint.footprint.classes.custom.CustomBarChartRender
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
@@ -22,69 +22,7 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding
     override fun initAfterBinding() {
         setBinding()
 
-        val entries = arrayListOf<BarEntry>(
-            BarEntry(1f, 20f),
-            BarEntry(2f, 40f),
-            BarEntry(3f, 40f),
-            BarEntry(4f, 80f),
-            BarEntry(5f, 50f),
-            BarEntry(6f, 20f),
-            BarEntry(7f, 10f),
-        )
-
-        binding.mypageBarChartBc.apply {
-            description.isEnabled = false
-            legend.isEnabled = false
-            axisRight.isEnabled = false
-            setScaleEnabled(false)
-            setPinchZoom(false)
-            setDrawGridBackground(false)
-            setTouchEnabled(false)
-            axisLeft.apply {
-                axisMaximum = 101f
-                axisMinimum = 0f
-                granularity = 20f
-                setDrawLabels(false) // y축 값
-                setDrawGridLines(false)
-                setDrawAxisLine(false)
-            }
-            xAxis.apply {
-                position = XAxis.XAxisPosition.BOTTOM
-                textSize = 12f
-                valueFormatter = XAxisFormatter()
-                granularity = 1f
-                setDrawLabels(true) // x축 값
-                setDrawGridLines(false)
-                setDrawAxisLine(false)
-            }
-        }
-
-        val set = BarDataSet(entries, "DataSet")
-        set.color = requireContext().getColor(R.color.primary)
-        set.setDrawValues(false)
-
-        val dataSet = arrayListOf<IBarDataSet>(set)
-        val data = BarData(dataSet)
-        data.barWidth = 0.5f
-
-        binding.mypageBarChartBc.run {
-            this.data = data
-            setFitBars(true)
-            invalidate()
-        }
-
-        binding.mypageGoalLeftIv.setOnClickListener {
-            findNavController().navigate(R.id.action_mypageFragment_to_badgeFragment)
-        }
-
-        //설정 버튼 클릭 리스너 -> SettingFragment 로 이동
-        binding.mypageSearchIv.setOnClickListener {
-            findNavController().navigate(R.id.action_mypageFragment_to_navigation)
-        }
-
-        binding.mypageTmpGoalTv.setOnClickListener { //이번달 목표 텍스트뷰 클릭 리스너 -> GoalThisMonthFragment 로 이동(임시)
-            findNavController().navigate(R.id.action_mypageFragment_to_goalThisMonthFragment)
-        }
+        initWeekGraph()
     }
 
     private fun setBinding() {
@@ -114,6 +52,97 @@ class MyPageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding
         binding.mypageGoalDayTv.text = spannableDay
 
         binding.mypageStatisticsResult2Tv.text = " 월요일"
+
+        binding.mypageGoalLeftIv.setOnClickListener {
+            findNavController().navigate(R.id.action_mypageFragment_to_badgeFragment)
+        }
+    }
+
+    private fun initWeekGraph() {
+        val colorGray = requireContext().getColor(R.color.white_caption)
+
+        // 커스텀 막대 radius 설정
+        val barChartRender =
+            CustomBarChartRender(
+                binding.mypageBarChartBc,
+                binding.mypageBarChartBc.animator,
+                binding.mypageBarChartBc.viewPortHandler
+            )
+        barChartRender.setRadius(30)
+
+        // 그래프 설정
+        binding.mypageBarChartBc.apply {
+            description.isEnabled = false
+            legend.isEnabled = false
+            axisRight.isEnabled = false
+            setScaleEnabled(false)
+            setPinchZoom(false)
+            setDrawGridBackground(false)
+            setTouchEnabled(false)
+            setPadding(16, 10, 16, 10)
+            extraBottomOffset = 15f
+            renderer = barChartRender
+            axisLeft.apply {
+                axisMaximum = 101f
+                axisMinimum = 0f
+                granularity = 20f
+                textSize = 10f
+                typeface = resources.getFont(R.font.namusquareround_extra_bold)
+                textColor = colorGray
+                gridColor = colorGray
+                gridLineWidth = 1f
+                setDrawLabels(true) // y축 값
+                setDrawGridLines(true)
+                setDrawAxisLine(false)
+            }
+            xAxis.apply {
+                yOffset = 10f
+                position = XAxis.XAxisPosition.BOTTOM
+                textColor = requireContext().getColor(R.color.black_dark)
+                textSize = 12f
+                typeface = resources.getFont(R.font.namusquareround_bold)
+                valueFormatter = XAxisFormatter()
+                granularity = 1f
+                setDrawLabels(true) // x축 값
+                setDrawGridLines(false)
+                setDrawAxisLine(false)
+            }
+        }
+
+        // 데이터 설정
+        setWeekGraphData()
+    }
+
+    private fun setWeekGraphData() {
+        // 값
+        val weekRate = arrayListOf<Float>(20f, 40f, 40f, 80f, 50f, 20f, 10f)
+        val maxIdx = weekRate.indexOf(weekRate.maxOrNull())
+
+        val entries = arrayListOf<BarEntry>()
+        val colors = arrayListOf<Int>()
+
+        for (idx in 0 until weekRate.size) {
+            entries.add(BarEntry((idx + 1).toFloat(), weekRate[idx]))
+            if (maxIdx != idx) {
+                colors.add(requireContext().getColor(R.color.primary))
+            } else {
+                colors.add(requireContext().getColor(R.color.secondary))
+            }
+        }
+
+        val set = BarDataSet(entries, "DataSet")
+        set.colors = colors
+        set.setDrawValues(false)
+
+        val dataSet = arrayListOf<IBarDataSet>(set)
+        val data = BarData(dataSet)
+        data.barWidth = 0.4f
+
+        binding.mypageBarChartBc.run {
+            this.data = data
+            setFitBars(true)
+            invalidate()
+        }
     }
 
     inner class XAxisFormatter : ValueFormatter() {
